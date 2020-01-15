23ccc506763086ee9f4f40b6e71e001a867ccb9f
a819721810 <819721810@qq.com>
2020-01-15 21:31:22 +0800

    标准的hash桶解法

--- JavaLeetCode/src/LongestSubstringWithoutRepeatingCharacters.java
+++ JavaLeetCode/src/LongestSubstringWithoutRepeatingCharacters.java
@@ -11,28 +11,40 @@ class Solution {
         {
             return 1;
         }
-        HashMap<Character,Integer> hashMap=new HashMap<>();
-        int start=0,end=0;
-        int maxLen = 0;
-        for (;end<s.length();)
+        int len=s.length();
+        int maxLen=0;
+        int []index=new int[128];
+        int i=0,j=0;
+        for (;j<len;j++)
         {
-
-            if (hashMap.get(s.charAt(end))!=null&&hashMap.get(s.charAt(end))>=start)
-            {
-                start=hashMap.get(s.charAt(end))+1;
-            }
-            else
-            {
-                hashMap.put(s.charAt(end),end);
-                end++;
-                int sLen = end - start;
-                if (sLen > maxLen)
-                {
-                    maxLen = sLen;
-                }
-            }
+            i=Math.max(index[s.charAt(j)],i);
+            maxLen=Math.max(maxLen,j-i+1);
+            index[s.charAt(j)]=j+1;
         }
+
         return maxLen;
+//        HashMap<Character,Integer> hashMap=new HashMap<>();
+//        int start=0,end=0;
+//        int maxLen = 0;
+//        for (;end<s.length();)
+//        {
+//
+//            if (hashMap.get(s.charAt(end))!=null&&hashMap.get(s.charAt(end))>=start)
+//            {
+//                start=hashMap.get(s.charAt(end))+1;
+//            }
+//            else
+//            {
+//                hashMap.put(s.charAt(end),end);
+//                end++;
+//                int sLen = end - start;
+//                if (sLen > maxLen)
+//                {
+//                    maxLen = sLen;
+//                }
+//            }
+//        }
+//        return maxLen;
     }
 }
 
