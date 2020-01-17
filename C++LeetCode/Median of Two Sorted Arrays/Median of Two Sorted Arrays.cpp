befd26fff233c65044a7098b01eb13b57877560b
a819721810 <819721810@qq.com>
2020-01-17 23:45:21 +0800

    使用排序，不使用O(N)空间

--- C++LeetCode/Median of Two Sorted Arrays/Median of Two Sorted Arrays.cpp
+++ C++LeetCode/Median of Two Sorted Arrays/Median of Two Sorted Arrays.cpp
@@ -5,29 +5,131 @@ using namespace std;
 class Solution {
 public:
 	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-		std::vector<int> finalVec;
-		std::multiset<int> temps;
-		for (const auto &num : nums1)
+		int nums1Size = nums1.size();
+		int nums2Size = nums2.size();
+		int allSize = nums1Size + nums2Size;
+		int midNum = allSize / 2;
+		int count = 0;
+		bool isOddNumber = true;
+		int midFirst = 0, midSecond = 0;
+		if (allSize % 2 == 0)
 		{
-			temps.emplace(num);
+			isOddNumber = false;
 		}
-		for (const auto &num : nums2)
+		int i = 0, j = 0;
+		while (i < nums1Size || j < nums2Size)
 		{
-			temps.emplace(num);
+			if (isOddNumber)
+			{
+				if (i < nums1Size && j < nums2Size)
+				{
+					if (nums1[i] > nums2[j])
+					{
+						if (count == midNum)
+						{
+							return nums2[j];
+						}
+						count++;
+						j++;
+					}
+					else
+					{
+						if (count == midNum)
+						{
+							return nums1[i];
+						}
+						count++;
+						i++;
+					}
+				}
+				else if (i < nums1Size)
+				{
+					if (count == midNum)
+					{
+						return nums1[i];
+					}
+					count++;
+					i++;
+				}
+				else
+				{
+					if (count == midNum)
+					{
+						return nums2[j];
+					}
+					count++;
+					j++;
+				}
+			}
+			//1 2 3 4 第一个target是4/2 -1 第二个是4/2
+			else
+			{
+				if (i < nums1Size && j < nums2Size)
+				{
+					if (nums1[i] > nums2[j])
+					{
+						if (count == midNum - 1)
+						{
+							/*return nums2[j];*/
+							midFirst = nums2[j];
+						}
+						else if (count == midNum)
+						{
+							midSecond = nums2[j];
+							return (midFirst + midSecond) / 2.0;
+						}
+						count++;
+						j++;
+					}
+					else
+					{
+						if (count == midNum - 1)
+						{
+							/*return nums2[j];*/
+							midFirst = nums1[i];
+						}
+						else if (count == midNum)
+						{
+							midSecond = nums1[i];
+							return (midFirst + midSecond) / 2.0;
+						}
+						count++;
+						i++;
+					}
+				}
+				else if (i < nums1Size)
+				{
+					if (count == midNum - 1)
+					{
+						/*return nums2[j];*/
+						midFirst = nums1[i];
+					}
+					else if (count == midNum)
+					{
+						midSecond = nums1[i];
+						return (midFirst + midSecond) / 2.0;
+					}
+					count++;
+					i++;
+				}
+				else
+				{
+					if (count == midNum-1)
+					{
+						midFirst = nums2[j];
+						//return nums2[j];
+					}
+					else if (count == midNum)
+					{
+						midSecond = nums2[j];
+						return (midFirst + midSecond) / 2.0;
+					}
+					count++;
+					j++;
+				}
+			}
 		}
-		for (const auto &temp : temps)
-		{
-			finalVec.emplace_back(temp);
-		}
-		auto finalVecSize = finalVec.size();
-		if (finalVecSize % 2 == 1)
-		{
-			return finalVec[finalVecSize / 2];
-		}
-		else
-		{
-			return (finalVec[finalVecSize / 2 - 1] + finalVec[finalVecSize / 2]) / 2.0;
-		}
+		return (midFirst + midSecond) / 2.0;
 	}
 };
 
